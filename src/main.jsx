a18e21e5cea822e34aaedd64cbae55b30f4705e7
roman-rsp <roman@rsp-verlag.com>
2026-02-05 11:56:32 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+/**
+ * RSP KUNSTVERLAG - Einstiegspunkt
+ * Diese Datei verbindet den Programmier-Code mit der HTML-Seite.
+ */
+
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error("Kritischer Fehler: Das Element 'root' wurde nicht gefunden.");
+}
